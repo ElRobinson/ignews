2136962033a17e22366b223bdad8a283cf26d77e
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2021-05-23 21:24:43 -0300

    feat: studies on nexr

---
+++ ignews/src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/Document';
+
+export default class MyDocument extends Document {
+    render () {
+        return (
+            <Html>
+                <Head>
+                    
+                    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"></link>
+                    <title>ig.news</title>
+                </Head>
+                <body>
+                    <Main/>
+                    <NextScript/>
+                </body>
+            </Html>
+        )
+    }
+} 
