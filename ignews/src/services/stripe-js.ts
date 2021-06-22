c480a34357a43c5692e01b318fd1b9066d3380ca
Luis Felipe Robinson <luisfelipe.robinson@gmail.com>
2021-06-21 21:44:41 -0300

    feat: added services and user create validation

---
+++ ignews/src/services/stripe-js.ts
@@ -0,0 +1,6 @@
+import { loadStripe } from '@stripe/stripe-js'
+
+export async function getStripe() {
+    const stripeJs = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY)
+    return stripeJs;
+}
