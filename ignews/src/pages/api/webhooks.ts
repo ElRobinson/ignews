65726c1170d63dba38dfff288d9e477d1d8c28fa
Luis Felipe Robinson <luisfelipe.robinson@gmail.com>
2021-06-25 19:45:15 -0300

    feat: added webhook

---
+++ ignews/src/pages/api/webhooks.ts
@@ -0,0 +1,56 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Readable } from "stream";
+import Stripe from "stripe";
+import { stripe } from "../../services/stripe";
+
+async function buffer(readable: Readable) {
+    //pedaços do stream
+    const chunks = [];
+
+    for await (const chunk of readable) {
+        chunks.push(
+            typeof chunk === "string" ? Buffer.from(chunk) : chunk
+        );
+    }
+
+    return Buffer.concat(chunks);
+    
+}
+
+export const config = {
+    api: {
+        bodyParser: false
+    }
+}
+
+const relevantEvents = new Set([
+    'checkout.session.completed'
+])
+
+export default async(req: NextApiRequest, res: NextApiResponse) => {
+    if ( req.method === 'POST') { 
+        const buff = await buffer(req)
+        const secret = req.headers['stripe-signature']
+
+        let event: Stripe.Event;
+
+        try {
+            event = stripe.webhooks.constructEvent(buff, secret, process.env.STRIPE_WEBHOOK_KEY);
+        } catch (err) {
+            return res.status(400).send(`Webhook error: ${err.message}`);
+        }
+
+        const type = event.type
+
+        if (!relevantEvents.has(type)){
+
+        }
+
+        res.status(200).json({ received:true})
+    } else {
+        res.setHeader('Allow', 'POST')
+        res.status(405).end('Method not allowed')
+    }  
+
+    
+}
