7fcb95703eaf4d26d210ea20ed200a9e3065152c
Luis Felipe Robinson <luisfelipe.robinson@gmail.com>
2021-07-14 21:14:21 -0300

    feat: added validation to diferent changes on user subscription

--- ignews/src/pages/api/webhooks.ts
+++ ignews/src/pages/api/webhooks.ts
@@ -25,7 +25,9 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-    'checkout.session.completed'
+    'checkout.session.completed',    
+    'customer.subscriptions.updated',
+    'customer.subscriptions.deleted',    
 ])
 
 export default async(req: NextApiRequest, res: NextApiResponse) => {
@@ -45,14 +47,27 @@ export default async(req: NextApiRequest, res: NextApiResponse) => {
 
         if (!relevantEvents.has(type)){
             try {
-                switch (type) {
+                switch (type) {                    
+                    case 'customer.subscriptions.updated':
+                    case 'customer.subscriptions.deleted':
+
+                        const subscription = event.data.object as Stripe.Checkout.Session;
+
+                        await saveSubscription(
+                            subscription.id,
+                            subscription.customer.toString(),
+                            type === 'customer.subscriptions.created',
+                        );
+
+                        break;
                     case 'checkout.session.completed':
 
                         const checkoutSession = event.data.object as Stripe.Checkout.Session
 
                         await saveSubscription(
                             checkoutSession.subscription.toString(),
-                            checkoutSession.customer.toString()
+                            checkoutSession.customer.toString(),
+                            true
                         )
 
                         break;
