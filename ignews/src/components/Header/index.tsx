001510d9dc6591acb5c45ebe3a1ffd14a9f258af
Luis Felipe Robinson <luisfelipe.robinson@gmail.com>
2021-07-25 18:39:17 -0300

    feat: added activeLink component and validations to return from prisma

--- ignews/src/components/Header/index.tsx
+++ ignews/src/components/Header/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { SignInButton } from '../SignInButton';
-import styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink';
 
-export function Header(){
+import styles from './styles.module.scss';
+//prefetch deixa a página pré carregada
+
+export function Header(){    
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                        <a>Home</a>
+                    </ActiveLink>                    
+                    <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
                 
                 <SignInButton/>
--- ignews/src/pages/posts/index.tsx
+++ ignews/src/pages/posts/index.tsx
@@ -2,10 +2,21 @@ import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../../services/prismic';
+import { RichText } from 'prismic-dom'
 
 import styles from './styles.module.scss'
 
-export default function Posts() {
+type Post = {
+    slug: string,
+    title: string,
+    excerpt: string, 
+    updatedAt: string,
+}
+interface PostsProps {
+    props:Post
+}
+
+export default function Posts({ posts }: PostsProps) {
     return (
         <>
             <Head>
@@ -14,31 +25,13 @@ export default function Posts() {
 
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a href="#">
-                        <time></time>
-                        <strong></strong>
-                        <p></p>
-                    </a>
-                    <a href="#">
-                        <time></time>
-                        <strong></strong>
-                        <p></p>
-                    </a>
-                    <a href="#">
-                        <time></time>
-                        <strong></strong>
-                        <p></p>
-                    </a>
-                    <a href="#">
-                        <time></time>
-                        <strong></strong>
-                        <p></p>
-                    </a>
-                    <a href="#">
-                        <time></time>
-                        <strong></strong>
-                        <p></p>
-                    </a>
+                    { posts.map(post => (
+                         <a key={post.slug} href="#">
+                         <time>{ post.updatedAt }</time>
+                         <strong> { post.title }</strong>
+                         <p> { post.excerpt }</p>
+                     </a>
+                    ))} 
                 </div>
             </main>
         </>
@@ -49,14 +42,27 @@ export default function Posts() {
 export const getStaticProps: GetStaticProps = async () => {
     const prismic = getPrismicClient()
 
-    const reponse = await prismic.query([
+    const response = await prismic.query([
         Prismic.predicates.at('document.type','publication')
     ], {
         fetch: ['publication.title', 'publication.content'],
         pageSize: 100, 
     })
 
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR',{
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric',
+            })
+        };
+    });
+
     return {
-        props: {}
+        props: {posts}
     }
 }
