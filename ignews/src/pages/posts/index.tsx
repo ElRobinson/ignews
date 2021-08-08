c50d9b029fab587a3cab9baa7c5b451abdbf45d1
Luis Felipe Robinson <luisfelipe.robinson@gmail.com>
2021-08-08 20:00:56 -0300

    added slug for posts and styles

--- ignews/src/pages/posts/index.tsx
+++ ignews/src/pages/posts/index.tsx
@@ -5,6 +5,7 @@ import { getPrismicClient } from '../../services/prismic';
 import { RichText } from 'prismic-dom'
 
 import styles from './styles.module.scss'
+import Link from 'next/link';
 
 type Post = {
     slug: string,
@@ -13,7 +14,7 @@ type Post = {
     updatedAt: string,
 }
 interface PostsProps {
-    props:Post
+    props:Post[]
 }
 
 export default function Posts({ posts }: PostsProps) {
@@ -26,11 +27,13 @@ export default function Posts({ posts }: PostsProps) {
             <main className={styles.container}>
                 <div className={styles.posts}>
                     { posts.map(post => (
-                         <a key={post.slug} href="#">
-                         <time>{ post.updatedAt }</time>
-                         <strong> { post.title }</strong>
-                         <p> { post.excerpt }</p>
-                     </a>
+                        <Link href={`/posts/${post.slug}`}>
+                            <a key={post.slug} >
+                                <time>{ post.updatedAt }</time>
+                                <strong> { post.title }</strong>
+                                <p> { post.excerpt }</p>
+                            </a>
+                        </Link>
                     ))} 
                 </div>
             </main>
